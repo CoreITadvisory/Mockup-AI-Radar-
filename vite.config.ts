ee069b8a7efdce6c9cdd12b4cc18977aa36db6a7
Leopold Michalewicz <leopold.michalewicz@coreitadvisory.com>
2025-04-01 12:51:45 +0200

    Update Vite config for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -29,9 +29,8 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
